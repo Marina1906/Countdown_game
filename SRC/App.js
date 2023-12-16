570c0d9bf850ee1eceb5a1ab3a23e8d7b1f7b203
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-12-16 20:45:09 +0100

    edited code

--- SRC/App.js
+++ SRC/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "App.css";
 const App = () => (
 
    <div>
-    <h1>Hello React</h1>
+    <h1 className="wow">Hello React</h1>
    </div>   
 );
 export default App;
